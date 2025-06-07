559d2d16d6795a2eee65e2a30ad433759f45cbc6
ronaldo <ronaldo@eero.com>
2025-06-07 06:00:19 -0300

    removing mock server

--- test/locust_olap_main.py
+++ test/locust_olap_main.py
@@ -1,16 +1,8 @@
-from locust import events, task
+from locust import task
 
-import gevent
 import grpc_user
 import olap_pb2
 import olap_pb2_grpc
-from hello_server import start_server
-
-
-# Start the dummy server. This is not something you would do in a real test.
-@events.init.add_listener
-def run_grpc_server(environment, **_kwargs):
-    gevent.spawn(start_server)
 
 
 class HelloGrpcUser(grpc_user.GrpcUser):
